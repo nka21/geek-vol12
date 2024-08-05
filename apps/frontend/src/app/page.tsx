b6407c2a18dd56253c2c4fefabe76c8cd49e19e9
nka21 <naoki.nico@gmail.com>
2024-08-06 00:14:13 +0900

    chore: 開発時の仮としてログインボタンの作成

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main>hello world!</main>;
+  return (
+    <main>
+      <header className="w-full bg-zinc-700 p-4 flex justify-between items-center">
+        <div className="text-xl font-bold">My App</div>
+        <Button variant="outline">ログイン</Button>
+      </header>
+    </main>
+  );
 }
