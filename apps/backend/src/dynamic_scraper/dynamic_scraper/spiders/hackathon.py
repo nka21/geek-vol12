8d80f43d3ad89c2592bb22b72e37feb59a9aba4a
nonlem <nonlem7432@gmail.com>
2024-08-09 15:29:56 +0900

    feat: Implement start_requests function to sequentially generate requests from URLs
    - Added start_requests method to iterate over a list of URLs and generate Scrapy requests.
    - Ensured that requests are processed in the specified order.
    - This enhancement allows for more controlled and sequential scraping of target URLs.

--- apps/backend/src/dynamic_scraper/dynamic_scraper/spiders/hackathon.py
+++ apps/backend/src/dynamic_scraper/dynamic_scraper/spiders/hackathon.py
@@ -3,8 +3,13 @@ import scrapy
 
 class HackathonSpider(scrapy.Spider):
     name = "hackathon"
-    allowed_domains = ["talent.supporterz.jp"]
-    start_urls = ["https://talent.supporterz.jp/geekcamp/"]
+
+    def start_requests(self):
+        allowed_domains = ["talent.supporterz.jp"]
+        urls = ["https://talent.supporterz.jp/geekcamp/"]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         pass
